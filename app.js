72a91512bc4faab88ec9b601b8f7bde228e278fd
Rebecca Meyer <faustorebeccabsit@gmail.com>
2021-01-02 11:40:06 +0100

    creating functions in obtaining data from fetch api and displaying the result

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const filterBox = document.querySelector('.job-filter');
 const emptyBox = document.querySelector('.empty-box');
+const container = document.querySelector('.container');
+
 
 let emptyBoxHeight = filterBox.clientHeight;
 
@@ -13,16 +15,67 @@ emptyBox.style.height = `${emptyBoxHeight + 60}px`;
 window.addEventListener('resize', resizeBox);
 
 function getJobPosts(){
+   const htmlPost = ``;
 
   let promiseJob = fetch('http://127.0.0.1:5500/data.json');
-
   let promiseJob2 = promiseJob.then( response => {
       return response.json();
    });
    // console.log(promiseJob2);
 
    promiseJob2.then(data => {
-      data.map( list => console.log(list));
+      generateHTML(data);
+      
+   });
+   promiseJob2.catch( error => {
+      console.log('Failed to fetch page: ', err)
+   })
+
+}
+
+function generateHTML(list){
+   let htmlJobPostBox = '';
+   list.map( listPost => {
+      htmlJobPostBox = 
+      `
+      <div class="job-posts">
+        <div class="logo">
+          <img src="${listPost.logo}">
+        </div>
+        <div class="content">
+          <div class="status">
+            <p>${listPost.company}</p>
+            <span>${listPost.new ? 'New': ''}</span>
+            <span>${listPost.featured ? 'Featured': ''}</span>
+          </div>
+          <p>${listPost.position}</p>
+          <div class="details">
+            <ul>
+               <li>${listPost.postedAt}</li>
+               <li>${listPost.contract}</li>
+               <li>${listPost.location}</li>
+            </ul>
+          </div>
+        </div>
+        <div class="languages">
+            <ul>
+               <li>${listPost.role}</li>
+               <li>${listPost.level}</li>
+               ${getLanguages(listPost.languages)}
+            </ul>
+        </div>
+      </div>
+      
+      `
+      container.insertAdjacentHTML('beforeend', htmlJobPostBox);
+   })
+
+}
+
+function getLanguages(data) {
+   data.map( language => {
+      console.log(language);
+      return `<li>${language}</li>`;
       
    });
 }
